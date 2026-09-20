3a67933f45bd3377d9c83b46ce3788ee5d7a6597
antonin-leroy <antonin.leroy02@gmail.com>
2026-09-20 15:28:55 +0200

    adaptation for our case

--- source/g1_fall_recovery/g1_fall_recovery/tasks/manager_based/g1_fall_recovery/g1_fall_recovery_env_cfg.py
+++ source/g1_fall_recovery/g1_fall_recovery/tasks/manager_based/g1_fall_recovery/g1_fall_recovery_env_cfg.py
@@ -82,23 +82,6 @@ class MySceneCfg(InteractiveSceneCfg):
 ##
 
 
-@configclass
-class CommandsCfg:
-    """Command specifications for the MDP."""
-
-    base_velocity = mdp.UniformVelocityCommandCfg(
-        asset_name="robot",
-        resampling_time_range=(10.0, 10.0),
-        rel_standing_envs=0.02,
-        rel_heading_envs=1.0,
-        heading_command=True,
-        heading_control_stiffness=0.5,
-        debug_vis=True,
-        ranges=mdp.UniformVelocityCommandCfg.Ranges(
-            lin_vel_x=(-1.0, 1.0), lin_vel_y=(-1.0, 1.0), ang_vel_z=(-1.0, 1.0), heading=(-math.pi, math.pi)
-        ),
-    )
-
 
 @configclass
 class ActionsCfg:
@@ -227,6 +210,10 @@ class RewardsCfg:
     """Reward terms for the MDP."""
 
     # -- task
+    track_height = RewTerm(
+        func=mdp.track_height, weight=1.0, params={"command_name": "base_velocity", "std": math.sqrt(0.25)}
+    )
+
     track_lin_vel_xy_exp = RewTerm(
         func=mdp.track_lin_vel_xy_exp, weight=1.0, params={"command_name": "base_velocity", "std": math.sqrt(0.25)}
     )
--- source/g1_fall_recovery/g1_fall_recovery/tasks/manager_based/g1_fall_recovery/mdp/rewards.py
+++ source/g1_fall_recovery/g1_fall_recovery/tasks/manager_based/g1_fall_recovery/mdp/rewards.py
@@ -25,3 +25,14 @@ def joint_pos_target_l2(env: ManagerBasedRLEnv, target: float, asset_cfg: SceneE
     joint_pos = wrap_to_pi(asset.data.joint_pos[:, asset_cfg.joint_ids])
     # compute the reward
     return torch.sum(torch.square(joint_pos - target), dim=1)
+
+
+def track_height(
+    env: ManagerBasedRLEnv, std: float, command_name: str,target_height: float, asset_cfg: SceneEntityCfg = SceneEntityCfg("robot")
+) -> torch.Tensor:
+    """Reward tracking of height."""
+    # extract the used quantities (to enable type-hinting)
+    asset: RigidObject = env.scene[asset_cfg.name]
+    # compute the error
+    lin_height_error = torch.square(env.command_manager.get_command(command_name)[:, 3] - asset.data.root_lin_vel_b[:, :2])
+    return torch.exp(-lin_vel_error / std**2)
